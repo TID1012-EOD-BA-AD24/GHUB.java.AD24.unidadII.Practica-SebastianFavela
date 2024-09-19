2159a6d3a603d5109bb6a76026df43bf19acacd8
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-09-19 19:10:47 +0000

    Clase Delimitadores

--- miTest/AppTest.java
+++ miTest/AppTest.java
@@ -5,11 +5,39 @@ import miPrincipal.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 class AppTest {
 
-    @Test 
-    void appHasAGreeting() {
-        Principal classUnderTest = new Principal();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    
+    private Delimitadores objDel;
+
+    @BeforeEach
+    public void setUp() {
+        objDel = new Delimitadores();
+    }
+
+    @Test
+    public void testEvaluacionDelimitadores_ExpresionCorrecta() {
+        String expr = "while (m<(n[8]+0)) { int p=7; /*comentarios*/ }";
+        assertTrue(objDel.evaluacionDelimitadores(expr), "La expresión debería ser correcta");
+    }
+
+    @Test
+    public void testEvaluacionDelimitadores_ExpresionIncorrecta() {
+        String expr = "while (m<(n(8]+0)) {{ int p=7; /*comentarios*/ }";
+        assertFalse(objDel.evaluacionDelimitadores(expr), "La expresión debería ser incorrecta");
+    }
+
+    @Test
+    public void testEvaluacionDelimitadores_ExpresionVacia() {
+        String expr = "";
+        assertTrue(objDel.evaluacionDelimitadores(expr), "La expresión vacía debería ser correcta");
+    }
+
+    @Test
+    public void testEvaluacionDelimitadores_ExpresionConSoloDelimitadores() {
+        String expr = "{}[]()";
+        assertTrue(objDel.evaluacionDelimitadores(expr), "La expresión con solo delimitadores debería ser correcta");
     }
 }
