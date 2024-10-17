82378372ac8ddeb1fa651de5b42d8a4b23e1777d
Sebastian Favela Gaxiola <159967496+SebastianFavela@users.noreply.github.com>
2024-10-17 14:29:14 +0000

    .

---
+++ listaContactos/Contacto.java
@@ -0,0 +1,15 @@
+package listaContactos;
+    public class Contacto{
+        private string nombres;
+        private string apellido;
+        private string direccion;
+        private string correo;
+        private string telefono;
+        private string celular;
+
+        public Contacto(String nombres, String apellido, string direccion, string correo, string telefono, string celular){
+            
+
+        }
+
+    }
